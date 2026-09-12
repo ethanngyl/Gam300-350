937e1405ed929af5315bb11b6a121f8395116bd1
GabrielSebastianPutra <abrielsbtpt@gmail.com>
2026-09-12 09:18:16 +0800

    Basic Image Upload and FrontEnd - BackEnd connection
    1. Changed the structure of the project, both the Backend and Web-App files require the node-modules to be in the same root folder, so both files have been move to packages/src.
    2. I now know how to connect the frontend via react to the backend via node.js.
    3. I now know how to upload and store image files.
    4. I am still working on how to connect image uploading via frontend to backend.
    5. Do not pull unless you are working on frontend, thank you.

--- packages/vite.config.js
+++ packages/vite.config.js
@@ -1,7 +1,17 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Try to keep the vite.config.js and packages.json file as one without copying
+  root: path.resolve(__dirname, 'src/web-app'),
+  build: {
+    outDir: path.resolve(__dirname, 'src/web-app/dist'),
+    emptyOutDir: true,
+  },
 })
